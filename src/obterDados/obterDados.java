566bdf867f5ce7ba5d32a67cc6a5fb9f88cd2382
Paulo Vieira <paulo_vieira22@hotmail.com>
2024-01-27 09:36:52 -0300

    Initial commit

---
+++ src/obterDados/obterDados.java
@@ -0,0 +1,18 @@
+public String obterDados(String endereco) {
+    HttpClient client = HttpClient.newHttpClient();
+    HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(endereco))
+            .build();
+    HttpResponse<String> response = null;
+    try {
+        response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+    } catch (IOException e) {
+        throw new RuntimeException(e);
+    } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+    }
+
+    String json = response.body();
+    return json;
+}
